94644394fc7a34e8f305905cafe3a9278b3c894b
Kevin Huang <yellow.khaos@gmail.com>
2018-10-06 02:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 function driversWithRevenueOver(collection, number) {
   let newCollection = collection.filter(function(individual) {return individual.revenue > number})
-
+  return newCollection
 }
 
 function driverNamesWithRevenueOver() {
